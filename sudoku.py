6eb23048c00a43cd058775ffc9a3866554aecf24
Shixian Li <lsx7@sina.com>
2025-02-03 16:34:14 +0800

    Remove: 删除解锁和检查按钮

--- sudoku.py
+++ sudoku.py
@@ -37,26 +37,6 @@ class Sudoku:
         else:
             return []
 
-    def CheckRepeat(self, nums):
-        for num in range(1, 10):
-            if nums.count(num) > 1:
-                return num
-
-    def CheckError(self):
-        for i in range(9):
-            num = self.CheckRepeat(self.GetRow(i))
-            if num:
-                return f'Error: Multiple num {num} in row {i + 1}'
-            num = self.CheckRepeat(self.GetColumn(i))
-            if num:
-                return f'Error: Multiple num {num} in column {i + 1}'
-            num = self.CheckRepeat(self.GetBlock(i))
-            if num:
-                return f'Error: Multiple num {num} in block {i + 1}'
-
-    def CheckFinished(self):
-        return all(sum(self.data, [])) and not self.CheckError()
-
     def SolveOne(self):
         count = 0
         for i in range(81):
@@ -230,13 +210,13 @@ class NumBox(wx.Panel):
                 self.SetCell(r, c, data[r][c])
 
     def SetLock(self):
-        for item in self.GetItems():
-            if item.GetLabel():
-                item.Disable()
-
-    def SetUnlock(self):
-        for item in self.GetItems():
-            item.Enable()
+        if any(item.IsEnabled() and item.GetLabel() for item in self.GetItems()):
+            for item in self.GetItems():
+                if item.GetLabel():
+                    item.Disable()
+        else:
+            for item in self.GetItems():
+                item.Enable()
 
     def ClearUnlocked(self):
         for i, item in enumerate(self.GetItems()):
@@ -273,8 +253,6 @@ class NumBox(wx.Panel):
                                 self.SetCellColour(r, c, COLOUR_GREEN)
 
     def AutoComplete(self):
-        if self.sudoku.CheckError():
-            return "Exist error, can't auto complete."
         self.sudoku.Solve()
         self.SetData(self.sudoku.data)
 
@@ -292,10 +270,8 @@ class MyPanel(wx.Panel):
         self.SetSelection = self.numpad.SetSelection
 
         btn1 = wx.Button(self, -1, '锁定')
-        btn2 = wx.Button(self, -1, '解锁')
+        btn2 = wx.Button(self, -1, '自动')
         btn3 = wx.Button(self, -1, '清除')
-        btn5 = wx.Button(self, -1, '自动')
-        btn6 = wx.Button(self, -1, '检查')
 
         toolbar = wx.BoxSizer(wx.VERTICAL)
         toolbar.Add(self.numpad, 0, wx.EXPAND)
@@ -303,14 +279,10 @@ class MyPanel(wx.Panel):
         toolbar.Add(btn1, 0, wx.LEFT | wx.RIGHT | wx.BOTTOM | wx.EXPAND, 5)
         toolbar.Add(btn2, 0, wx.LEFT | wx.RIGHT | wx.BOTTOM | wx.EXPAND, 5)
         toolbar.Add(btn3, 0, wx.LEFT | wx.RIGHT | wx.BOTTOM | wx.EXPAND, 5)
-        toolbar.Add(btn5, 0, wx.LEFT | wx.RIGHT | wx.BOTTOM | wx.EXPAND, 5)
-        toolbar.Add(btn6, 0, wx.LEFT | wx.RIGHT | wx.BOTTOM | wx.EXPAND, 5)
 
         btn1.Bind(wx.EVT_BUTTON, lambda e: self.numbox.SetLock())
-        btn2.Bind(wx.EVT_BUTTON, lambda e: self.numbox.SetUnlock())
+        btn2.Bind(wx.EVT_BUTTON, lambda e: self.numbox.AutoComplete())
         btn3.Bind(wx.EVT_BUTTON, lambda e: self.numbox.ClearUnlocked())
-        btn5.Bind(wx.EVT_BUTTON, lambda e: self.numbox.AutoComplete())
-        btn6.Bind(wx.EVT_BUTTON, lambda e: wx.MessageBox(self.sudoku.CheckError() or 'No error found!', 'Error'))
 
         box = wx.BoxSizer()
         box.Add(self.numbox)
